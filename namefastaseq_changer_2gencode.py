75932952c7ba5b4ed1336881f309ea8dcc0479f5
João Raimundo <32943125+ray2g@users.noreply.github.com>
2019-11-03 14:59:04 +0000

    Update namefastaseq_changer_2gencode.py

--- namefastaseq_changer_2gencode.py
+++ namefastaseq_changer_2gencode.py
@@ -4,27 +4,37 @@ import argparse
 
 parser = argparse.ArgumentParser(prog='python3')
 
-# input file parser
+
 parser.add_argument("input_fasta", metavar="input.fasta", type= str,
                     help= "Fasta file to replace sequence names/description for a generate code, e.g. S001")
+      
+                     #input file parser
 
-#output file parser
+
 parser.add_argument("output_fasta", metavar="output.fasta", type= str,
                     help= "Output fasta file where the sequence description/name was changed to a generated code")
 
+                     #output file parser
+
+
 arguments = parser.parse_args()
 
-#generate the code for the sequence description assume
+
 def seq_number_gen(count):
+  """
+  generate the code for the sequence description assume
+  """
     seq_number= str(count)
     while len(seq_number) < 3:
         seq_number = "0" + seq_number
     return seq_number
 
-#change de sequence description/name to the generated code
-def function(input_file,output_file):
 
-    input = open(input_file, 'r') #open the input file
+def changer(input_file, output_file):
+  #change de sequence description/name to the generated code
+  
+  
+    file_handle = open(input_file, 'r') #open the input file
 
     output= open(output_file, 'w') #open the output file
 
@@ -39,15 +49,17 @@ def function(input_file,output_file):
             count+= 1
 
             new_seqname = '>S' + seq_number_gen(count)+'\n' 
-            # write on the new output file the trade of description | generated code
-            output.write(new_seqname)
+     
+            output.write(new_seqname) # write on the new output file the trade of description | generated code
             
-        # if the line dont start with ">" write the the sequence normaly
-        else:                      
+       
+        else:                         # if the line dont start with ">" write the the sequence normaly                   
             output.write(line)
             
-            # aggregate the sequence description with the correnpondent generated code
-            #dictio[new_seqname]=line[1:] 
+            """
+            aggregate the sequence description with the correnpondent generated code
+            dictio[new_seqname]=line[1:] 
+            """
 
-function(arguments.input_fasta,arguments.output_fasta)
+changer(arguments.input_fasta,arguments.output_fasta)
 
