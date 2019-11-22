29c32919dd4dffa7695ff2f5087fd0125368666b
João Raimundo <32943125+ray2g@users.noreply.github.com>
2019-11-22 15:51:15 +0000

    Create efetch.sh
    script for download sample allignments in fasta format

---
+++ efetch.sh
@@ -0,0 +1,1 @@
+wget https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi/\?db\=nucleotide\&id\= "ACESSION NUMBERS DO NCBI SEPARADOS POR VIGULAS"\&rettype\=fasta -O ~/"PATH PARA A DIRETORIA DO SAVE".fasta
